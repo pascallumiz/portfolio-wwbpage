464ca25ce644b340e9fb8946cbc7424775a258d9
Pascal Lumbasi <151160911+pascallumiz@users.noreply.github.com>
2024-09-12 13:43:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const typedText = document.querySelector(".hero-text h1");
 const words = ["Hello", "Welcome 😊"];
 let wordIndex = 0;
 let letterIndex = 0;
-let typingSpeed = 150;  // Speed of typing each letter
-let deletingSpeed = 100;  // Speed of deleting each letter
-let pauseDuration = 2000;  // Pause after completing each word
+let typingSpeed = 150;  
+let deletingSpeed = 100;  
+let pauseDuration = 2000; 
 
 function typeWord() {
     if (letterIndex < words[wordIndex].length) {
@@ -13,19 +13,19 @@ function typeWord() {
         letterIndex++;
         setTimeout(typeWord, typingSpeed);
     } else {
-        // Pause before deleting
+
         setTimeout(deleteWord, pauseDuration);
     }
 }
 
 function deleteWord() {
     if (letterIndex > 0) {
-        // Deleting letters
+    
         typedText.textContent = typedText.textContent.slice(0, letterIndex - 1);
         letterIndex--;
         setTimeout(deleteWord, deletingSpeed);
     } else {
-        // Move to the next word
+    
         wordIndex = (wordIndex + 1) % words.length;
         setTimeout(typeWord, 500);
     }
